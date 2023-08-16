57ecb9cab561ccf778216c95a190338f55bcf741
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-08-16 12:00:11 -0700

    Fix - Adding header Access-Control-Allow-Origin

--- src/pages/api/avo/index.js
+++ src/pages/api/avo/index.js
@@ -3,7 +3,7 @@ import Database from "@database/db";
 const allAvos = async(request, response) => {
   const database = new Database()
   const data = await database.getAll();
-  response.header('Access-Control-Allow-Origin', '*')
+  response.setHeader('Access-Control-Allow-Origin', '*');
   response.status(200).json({ length:data.length, data });
 }
 
