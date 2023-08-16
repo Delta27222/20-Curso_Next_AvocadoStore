d10a1016e3349303a2d0491963cb413ea248ee5f
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-08-16 12:14:36 -0700

    Fix - Adding header Access-Control-Allow-Origin

--- src/pages/api/avo/index.js
+++ src/pages/api/avo/index.js
@@ -1,11 +1,19 @@
 import Database from "@database/db";
 
- 
+const ACCEPTED_ORIGINS = [
+  'http://localhost:3000',
+  'https://avocado-store-27222.vercel.app'
+]
+
 const allAvos = async(request, response) => {
   const database = new Database()
   const data = await database.getAll();
   //Esto es necesario para que no de error de CORS, de esta manera indicamos quienes podran hacer peticiones a nuestra api
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  const origin = request.header('origin')
+  if(ACCEPTED_ORIGINS.includes(origin)){
+    response.setHeader('Access-Control-Allow-Origin', origin);
+
+  }
   response.status(200).json({ length:data.length, data });
 }
 
