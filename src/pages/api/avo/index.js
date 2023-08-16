b9a05e2a53ad68a8a814fb6c7cbc27e00e16ef3f
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-08-16 12:24:07 -0700

    Fix - Adding header Access-Control-Allow-Origin

--- src/pages/api/avo/index.js
+++ src/pages/api/avo/index.js
@@ -1,14 +1,10 @@
 import Database from "@database/db";
 
-
 const allAvos = async(request, response) => {
   const database = new Database()
   const data = await database.getAll();
   //Esto es necesario para que no de error de CORS, de esta manera indicamos quienes podran hacer peticiones a nuestra api
-  const origin = request.header('origin')
-  if(ACCEPTED_ORIGINS.includes(origin) || !origin){
-    response.setHeader('Access-Control-Allow-Origin', '*');
-  }
+  response.setHeader('Access-Control-Allow-Origin', '*');
   response.status(200).json({ length:data.length, data });
 }
 
